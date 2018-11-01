9e283ebec06399dbaf41be660a1a6b7a714f43a9
sbaturin <s.baturin@mobioinc.com>
2018-11-01 22:18:08 +0300

    isolation of view

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,29 +1,16 @@
 <?php
-
 $dbconn = pg_connect("host=db-dev.mobio.ru port=5432 dbname=dev_baturin_sf user=developer password=RuYPamZOPZ")
-    or die("Невозможно подключиться к БД");
-echo "Успешно подключено к БД";
+or die("Невозможно подключиться к БД");
 
 $result = pg_query($dbconn, 'SELECT id, title FROM post');
-?>
 
-<html>
-<head>
-    <title>List of Posts</title>
-</head>
-<body>
-<h1>List of Posts</h1>
-<ul>
-    <?php while ($row = pg_fetch_assoc($result)): ?>
-        <li>
-            <a href="/show.php?id=<?php echo $row['id'] ?> ">
-                <?php echo $row['title'] ?>
-            </a>
-        </li>
-    <?php endwhile; ?>
-</ul>
-</body>
-</html>
-
-<?php
+$posts = [];
+while ($row = pg_fetch_assoc($result)) {
+    $posts[] = $row;
+}
 pg_close($dbconn);
+
+unset($result);
+
+// include the HTML presentation code
+require '../templates/list.php';
