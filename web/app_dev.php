b866c3b90a4e16a025d0a8b50c252782012188c7
sbaturin <s.baturin@mobioinc.com>
2018-11-02 18:05:23 +0300

    mvc

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,16 +1,7 @@
 <?php
-$dbconn = pg_connect("host=db-dev.mobio.ru port=5432 dbname=dev_baturin_sf user=developer password=RuYPamZOPZ")
-or die("Невозможно подключиться к БД");
 
-$result = pg_query($dbconn, 'SELECT id, title FROM post');
+require_once 'model.php';
 
-$posts = [];
-while ($row = pg_fetch_assoc($result)) {
-    $posts[] = $row;
-}
-pg_close($dbconn);
+$posts = get_all_posts();
 
-unset($result);
-
-// include the HTML presentation code
 require '../templates/list.php';
---
+++ web/model.php
@@ -0,0 +1,29 @@
+<?php
+
+function open_database_connection()
+{
+    $dbconn = pg_connect("host=db-dev.mobio.ru port=5432 dbname=dev_baturin_sf user=developer password=RuYPamZOPZ")
+    or die("Невозможно подключиться к БД");
+
+    return $dbconn;
+}
+
+function close_database_connection($dbconn)
+{
+    pg_close($dbconn);
+}
+
+function get_all_posts()
+{
+    $dbconn = open_database_connection();
+    $result = pg_query($dbconn, 'SELECT id, title FROM post');
+
+    $posts = [];
+    while ($row = pg_fetch_assoc($result)) {
+        $posts[] = $row;
+    }
+    unset($result);
+    close_database_connection($dbconn);
+
+    return $posts;
+}
