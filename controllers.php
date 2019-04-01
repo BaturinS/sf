49c291ddb2773c5dadc4d3207d6014bd05bab85a
sbaturin <email@mobiogroup.com>
2019-04-01 21:14:32 +0300

    migrate to LAMP-server

--- controllers.php
+++ controllers.php
@@ -3,11 +3,11 @@
 function list_action()
 {
     $posts = get_all_posts();
-    require '../templates/list_of_posts.php';
+    require 'templates/list_of_posts.php';
 }
 
 function show_action($id)
 {
     $post = getPostById($id);
-    require '../templates/post.php';
+    require 'templates/post.php';
 }
--- model.php
+++ model.php
@@ -2,24 +2,24 @@
 
 function open_database_connection()
 {
-    $dbconn = pg_connect("host=db-dev.mobio.ru port=5432 dbname=dev_baturin_sf user=developer password=RuYPamZOPZ")
-    or die("Невозможно подключиться к БД");
+    $dbconn = mysqli_connect('localhost', 'admin', 'Mobio.');
+    mysqli_select_db($dbconn, 'sf');
 
     return $dbconn;
 }
 
 function close_database_connection($dbconn)
 {
-    pg_close($dbconn);
+    mysqli_close($dbconn);
 }
 
 function get_all_posts()
 {
     $dbconn = open_database_connection();
-    $result = pg_query($dbconn, 'SELECT id, title FROM post ORDER BY id');
+    $result = mysqli_query($dbconn, 'SELECT id, title FROM post ORDER BY id');
 
     $posts = [];
-    while ($row = pg_fetch_assoc($result)) {
+    while ($row = mysqli_fetch_assoc($result)) {
         $posts[] = $row;
     }
     unset($result);
@@ -31,9 +31,9 @@ function get_all_posts()
 function getPostById($id)
 {
     $dbconn = open_database_connection();
-    $result = pg_query($dbconn, "SELECT title, text, date FROM post WHERE id = $id");
+    $result = mysqli_query($dbconn, "SELECT title, text, date FROM post WHERE id = $id");
 
-    $post = pg_fetch_assoc($result);
+    $post = mysqli_fetch_assoc($result);
 
     close_database_connection($dbconn);
 
