5b05c895c7e069e5e63f8235828b6c45474e46d9
sbaturin <s.baturin@mobiogroup.com>
2019-05-09 04:57:08 +0300

    add render_template function and use response in controllers

--- controllers.php
+++ controllers.php
@@ -1,13 +1,30 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+
 function list_action()
 {
     $posts = get_all_posts();
-    require 'templates/list_of_posts.php';
+    $html = render_template('templates/list_of_posts.php', array('posts' => $posts));
+
+    return new Response($html);
 }
 
 function show_action($id)
 {
     $post = getPostById($id);
-    require 'templates/post.php';
+    $html = render_template('templates/post.php', array('post' => $post));
+
+    return new Response($html);
+}
+
+// Функция-помощник для отображения шаблонов
+function render_template($path, array $args)
+{
+    extract($args);
+    ob_start();
+    require $path;
+    $html = ob_get_clean();
+
+    return $html;
 }
